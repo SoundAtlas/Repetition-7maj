cdb96fbdee6a597a8377d386671db86e650196bd
Andreas Hemmer <andreas.hemmer@hotmail.com>
2026-05-07 09:24:26 +0200

    Add project files.

---
+++ Repetition 7-5/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Repetition_7_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 13;
+            int b = 4;
+            double c = 13;
+
+            Console.WriteLine(a / b);
+            Console.WriteLine(c / b);
+            Console.WriteLine(a % b);
+
+            string text = "C#";
+            int year = 2026;
+
+            Console.WriteLine($"Vi lærer {text} i {year}!");
+
+            bool isReady = a > 10 && b < 5;
+            bool isMaybe = a < 10 || b == 4;
+
+            Console.WriteLine($"{isReady}, {isMaybe}");
+        }
+    }
+}
